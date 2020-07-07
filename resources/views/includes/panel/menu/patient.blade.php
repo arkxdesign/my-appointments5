ab174efe74c6f3b41c3237e4b2e22b4e75a8bbce
arkxdesign <67521058+arkxdesign@users.noreply.github.com>
2020-07-06 20:24:02 -0500

    Schedule, Appointment an Doctor

--- resources/views/includes/panel/menu/patient.blade.php
+++ resources/views/includes/panel/menu/patient.blade.php
@@ -1,11 +1,11 @@
 <li class="nav-item">
-  <a class="nav-link" href="/appointments/create">
+  <a class="nav-link" href="{{ url('appointments/create' )}}">
     <i class="ni ni-send text-orange"></i>
     <span class="nav-link-blue">Reservar cita</span>
   </a>
 </li>
 <li class="nav-item">
-  <a class="nav-link" href="/appointments">
+  <a class="nav-link" href="{{ url('appointments' )}}">
     <i class="ni ni-time-alarm text-indigo"></i>
     <span class="nav-link-text">Mis citas</span>
   </a>
