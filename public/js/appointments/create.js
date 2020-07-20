d8448a54e8ed403063813c45700253d754f49e43
arkxdesign <a_r_k_x@hotmail.com>
2020-07-20 23:51:50 +0100

    alert day correction

--- public/js/appointments/create.js
+++ public/js/appointments/create.js
@@ -39,7 +39,9 @@
     }
 
     function displayHours(data){
-      if (!data.morning && !data.afternoon) {
+      if (!data.morning && !data.afternoon ||
+        data.morning.length===0 && data.afternoon.length===0) {
+
         $hours.html(noHoursAlert);
           return;
       }
