c14477e1b37e06a58022d93805d355e2669a274b
arkxdesign <a_r_k_x@hotmail.com>
2020-07-27 21:49:22 +0100

    Fix alert danger (appointments create) and appointment for API

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -9,19 +9,6 @@ use Auth;
 class AppointmentController extends Controller
 {
 
-/*
-"id": 1,
-"description": "Test",
-"speciality_id": 3,
-"doctor_id": 3,
-"patient_id": 2,
-"scheduled_date": "2020-07-30",
-"scheduled_time": "17:30:00",
-"type": "Consulta",
-"created_at": "2020-07-27 13:09:58",
-"updated_at": "2020-07-27 13:09:58",
-"status": "Reservada"
-*/
     public function index()
     {
     	$user = Auth::guard('api')->user();
