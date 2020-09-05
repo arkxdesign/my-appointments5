fecc91b8c95ca360ef680eace0251ef6d2e7dc70
arkxdesign <a_r_k_x@hotmail.com>
2020-09-05 19:48:56 +0100

    Allow immediately cancel for pending appointments 5

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -141,7 +141,7 @@ class AppointmentController extends Controller
 		$saved = $appointment->save(); // update
 
 		if ($saved)
-			$appointment->patient->sendFCM('Su cita se ha confiemado!');
+			$appointment->patient->sendFCM('Su cita se ha confirmado!');
 
 		$notification = 'La cita se ha confirmada correctamente.';
 		return redirect('/appointments')->with(compact('notification'));
